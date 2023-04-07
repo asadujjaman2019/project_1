f615386ebff4c7ebadf06fcb3ea2e75d37a1249f
shiplu <md.shiplu.1981@gmail.com>
2023-04-07 18:07:41 +0600

    v2:shiplu:4-7-23 6:06 pm

---
+++ search.py
@@ -0,0 +1,12 @@
+import os
+def find_files(filename, search_path):
+   result = []
+
+# Wlaking top-down from the root
+   for root, dir, files in os.walk(search_path):
+      if filename in files:
+         result.append(os.path.join(root, filename))
+         print("ok")
+   return result
+
+print(find_files("day1.txt","D:"))
