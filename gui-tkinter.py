cde806197d41f9bbc64b36f03c45738df000752c
shiplu <md.shiplu.1981@gmail.com>
2023-10-26 20:22:41 +0600

    v2:shiplu:4-24-2023 1:1 am

--- gui-tkinter.py
+++ gui-tkinter.py
@@ -1,13 +1,15 @@
 import tkinter
 from tkinter import ttk
 
-# maim frame
+# maim window
 window = tkinter.Tk()
 window.title("Data Entry Form")
 
+
+# main frame
 frame = tkinter.Frame(window)
 frame.pack()
-# main frame
+
 
 # Saving user information Label frame
 user_info_frame = tkinter.LabelFrame(frame, text="User information")
@@ -47,8 +49,41 @@ nationality_combobox.grid(row=3, column=1)
 for widget in user_info_frame.winfo_children():
     widget.grid_configure(padx=10, pady=5)
 # Add padding to all grid in parent user_info_frame
-
 # saving user information Label frame
 
+# saving course Label frame
+course_frame = tkinter.Label(frame)
+course_frame.grid(row=1, column=0, padx=20, pady=20, sticky="news")
+
+registration_label = tkinter.Label(course_frame, text="Registration status")
+registration_label.grid(row=0, column=0)
+
+registration_check = tkinter.Checkbutton(course_frame, text="Currently registered")
+registration_check.grid(row=1, column=0)
+
+number_of_course_label = tkinter.Label(course_frame, text="Completed courses")
+number_of_course_label.grid(row=0, column=1)
+
+number_of_course_spinbox = tkinter.Spinbox(course_frame, from_=0, to="infinity")
+number_of_course_spinbox.grid(row=1, column=1)
+
+
+# saving course Label frame
+
+# saving Accept term Label frame
+terms_frame = tkinter.LabelFrame(frame, text="Terms & Conditions")
+terms_frame.grid(row=2, column=0, sticky="news", padx=20, pady=20)
+
+term_check = tkinter.Checkbutton(terms_frame, text="I accept the terms & Conditions")
+term_check.grid(row=0, column=0)
+# saving Accept term Label frame
+
+# saving Button
+button = tkinter.Button(frame, text="Submit Data")
+button.grid(row=3, column=0, sticky="news")
+# saving button
+
+# Main frame
+# Main window
 window.mainloop()
 
